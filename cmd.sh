5a895e6809eb39f46968ec809d44c09655c552c4
DamScan <damsc@pm.me>
2022-07-30 00:26:41 +0200

    Chang inventory file by yaml in cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,3 +1,3 @@
 ## Add ssh on server for connect with ssh without password ( read main.env)
 # TODO make a playbook 4 this
-ansible -i main.inv -m lineinfile -a "path=/etc/sudoers line='dams ALL=(ALL:ALL) NOPASSWD: ALL'" --become-method=su --become --ask-become-pass all
+ansible -i inventory.yaml -m lineinfile -a "path=/etc/sudoers line='dams ALL=(ALL:ALL) NOPASSWD: ALL'" --become-method=su --become --ask-become-pass all
